f51f56b86ba6d311d38990fbbe1711c7729a2daa
Vadym Priadun <priadun.vv22@gmail.com>
2021-06-20 13:11:47 +0300

    testers page

--- consl.js
+++ consl.js
@@ -1,4 +1,21 @@
-let getSmth = 2;
-  console.log (getSmth);
-let getNthg = 8
-  console.log (--getNthg)
+let money = prompt("Ваш бюджет на месяц?", "")
+let time = prompt("Введите дату в формате YYYY-MM-DD", "")
+
+let appData = {
+	budget: money,
+	expenses: {
+    "1000" : "300"
+  },
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+  appData.expenses.a1 = a2;
+  appData.expenses.a3 = a4;
+  alert(appData.budget / 30);
+  console.log(money)
